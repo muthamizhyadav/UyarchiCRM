76732dd4f6c4fcff3857496749849adc236b6664
vignesh449143 <101543057+vignesh449143@users.noreply.github.com>
2022-07-18 15:16:11 +0530

    wefef

--- src/services/requirementCollectionBS.service.js
+++ src/services/requirementCollectionBS.service.js
@@ -1184,10 +1184,12 @@ const updateRequirementSupplierById = async (supplierId, updateBody) => {
       },
     };
   }
+  if(data[0].moderatedPrice != null){
   if (updateBody.moderatedPrice != data[0].moderatedPrice) {
     // console.log('yes');
     values1 = { ...{ userId: data[0].userId, supplierReqId: data[0]._id, moderatedPrice: data[0].moderatedPrice, date:data[0].moderateDate, time:data[0].moderateTime} };
   }
+}
   if (
     data[0].expectedQnty != updateBody.expectedQnty &&
     data[0].expectedPrice == updateBody.expectedPrice &&
