387781257cf4e077e8866348cab3714fb80eaec0
unknown <rohitkumar05999@gmail.com>
2022-09-21 15:55:58 +0530

    rfgr

--- src/app.js
+++ src/app.js
@@ -22,13 +22,11 @@ const MessageRoute = require('./routes/v1/message.route');
 const httpServer = http.createServer(app);
 const { Messages } = require('../src/models/message.model');
 const moment = require('moment');
-
 const io = require('socket.io')(httpServer, {
   cors: {
     origin: '*',
   },
 });
-
 io.on('connection', (socket) => {
   const { roomId } = socket.handshake.query;
   socket.join(roomId);
@@ -37,73 +35,78 @@ io.on('connection', (socket) => {
     await Messages.create({ userId: userId, message: message, roomId: roomId, created: moment() });
     console.log(userId, message, roomId);
   });
-});
 
+  socket.on("callUser", ({ userToCall, signalData, from, name }) => {
+    io.to(userToCall).emit("callUser", {
+      signal: signalData,
+      from,
+      name,
+    });
+  });
+  socket.on("updateMyMedia", ({ type, currentMediaStatus }) => {
+    console.log("updateMyMedia");
+    socket.broadcast.emit("updateUserMedia", { type, currentMediaStatus });
+  });
+  socket.on("answerCall", (data) => {
+    socket.broadcast.emit("updateUserMedia", {
+      type: data.type,
+      currentMediaStatus: data.myMediaStatus,
+    });
+    io.to(data.to).emit("callAccepted", data);
+  });
+  socket.on("endCall", ({ id }) => {
+    io.to(id).emit("endCall");
+  });
+});
 // Socket Message Api's
 app.use('/meesageRoute', MessageRoute);
-
 if (config.env !== 'test') {
   app.use(morgan.successHandler);
   app.use(morgan.errorHandler);
 }
 // set security HTTP headers
 app.use(helmet());
-
 // parse json request body
 app.use(express.json());
 // parse urlencoded request body
 app.use(express.urlencoded({ extended: true }));
-
 // sanitize request data
 app.use(xss());
 app.use(mongoSanitize());
-
 // gzip compression
 app.use(compression());
-
 // enable cors
 app.use(cors());
 app.options('*', cors());
-
 // jwt authentication
 app.use(passport.initialize());
 passport.use('jwt', jwtStrategy);
-
 // limit repeated failed requests to auth endpoints
 if (config.env === 'production') {
   app.use('/v1/auth', authLimiter);
 }
-
 // v1 api routes
 app.use('/v1', routes);
-
 //default routes
-
 app.get('/', (req, res) => {
   res.sendStatus(200);
 });
 // default v1 route
-
 app.get('/v1', (req, res) => {
   res.sendStatus(200);
 });
-
 // health status code
 app.get('/health', (req, res) => {
   res.sendStatus(200);
 });
-
 // send back a 404 error for any unknown api request
 app.use((req, res, next) => {
   next(new ApiError(httpStatus.NOT_FOUND, 'Not found'));
 });
-
 // convert error to ApiError, if needed
 app.use(errorConverter);
-
 // handle error
 app.use(errorHandler);
-
 // mongoose connection
 mongoose.connect(config.mongoose.url, config.mongoose.options).then(() => {
   logger.info('Connected to MongoDB');
@@ -112,5 +115,4 @@ mongoose.connect(config.mongoose.url, config.mongoose.options).then(() => {
 httpServer.listen(config.port, () => {
   logger.info(`Listening to port ${config.port}`);
 });
-
 module.exports = app;
