072677c06774d46ea64146d86556b1e783776b27
muthamizhyadav <muthamizhyadav@gmail.com>
2022-12-02 16:06:46 +0530

    pagination

--- src/services/hostreg.service.js
+++ src/services/hostreg.service.js
@@ -1,5 +1,5 @@
 const httpStatus = require('http-status');
-const {Host, HostProduct, HostStreaming} = require('../models/hostreg.model');
+const { Host, HostProduct, HostStreaming } = require('../models/hostreg.model');
 const ApiError = require('../utils/ApiError');
 const Agora = require('agora-access-token');
 
@@ -8,29 +8,27 @@ const createHost = async (body) => {
 };
 
 const loginhostEmailAndPassword = async (email, mobileNumber) => {
-    const data = await Host.find({ email: email });
-    let number = data[0].mobileNumber
-    if (data != '') {
-      if (number == mobileNumber) {
-      } else {
-        throw new ApiError(httpStatus.NOT_FOUND, 'mobileNumber not Match');
-      }
+  const data = await Host.find({ email: email });
+  let number = data[0].mobileNumber;
+  if (data != '') {
+    if (number == mobileNumber) {
     } else {
-      throw new ApiError(httpStatus.NOT_FOUND, 'Email Not Registored');
+      throw new ApiError(httpStatus.NOT_FOUND, 'mobileNumber not Match');
     }
-  
-    return data;
-  };
+  } else {
+    throw new ApiError(httpStatus.NOT_FOUND, 'Email Not Registored');
+  }
+
+  return data;
+};
 
 const createHostProduct = async (body) => {
-  const data = await Host.findById(body.uid)
-  if(!data){
+  const data = await Host.findById(body.uid);
+  if (!data) {
     throw new ApiError(httpStatus.NOT_FOUND, 'User Not Found');
   }
   return HostProduct.create(body);
 };
-
-
 
 const createHostStreaming = async (userBody) => {
   const appID = 'dd80ee642fa84a36a365f560c3741929';
@@ -42,15 +40,13 @@ const createHostStreaming = async (userBody) => {
   const currentTimestamp = Math.floor(Date.now() / 1000);
   console.log(currentTimestamp);
   const expirationTimestamp = currentTimestamp + expirationTimeInSeconds;
-  const token = Agora.RtmTokenBuilder.buildToken(appID, appCertificate,userBody.selectHost, expirationTimestamp);
-  let values = { ...userBody, ...{token:token} };
+  const token = Agora.RtmTokenBuilder.buildToken(appID, appCertificate, userBody.selectHost, expirationTimestamp);
+  let values = { ...userBody, ...{ token: token } };
   return HostStreaming.create(values);
 };
 
-
-  
 const hostAll = async () => {
-  const data = await Host.find({category:"host"});
+  const data = await Host.find({ category: 'host' });
   return data;
 };
 
@@ -60,20 +56,20 @@ const getAll = async () => {
 };
 
 const RecipentAll = async () => {
-  const data = await Host.find({category:"recipient"});
+  const data = await Host.find({ category: 'recipient' });
   return data;
 };
 
-const getAllLiveStremingDatas = async () =>{
+const getAllLiveStremingDatas = async () => {
   const data = await HostStreaming.aggregate([
     {
       $lookup: {
         from: 'hosts',
         localField: 'selectHost',
-        pipeline:[
+        pipeline: [
           {
             $match: {
-              $and: [{ category: { $eq: "host" } }],
+              $and: [{ category: { $eq: 'host' } }],
             },
           },
         ],
@@ -83,118 +79,138 @@ const getAllLiveStremingDatas = async () =>{
     },
     { $unwind: '$hosts' },
     {
-      $project:{
-        hostname:"$hosts.name",
-        token:1,
-        allowChat:1,
-        participantAllowed:1,
-        endTime:1,
-        startTime:1,
-        stremingDate:1,
-        selectProduct:1,
-        selectHost:1,
-      }
-    }
-  ])
-  return data
-}
+      $lookup: {
+        from: 'hostproducts',
+        localField: 'selectProduct',
+        foreignField: '_id',
+        as: 'hostproducts',
+      },
+    },
+    {
+      $unwind: '$hostproducts',
+    },
+    {
+      $project: {
+        hostname: '$hosts.name',
+        token: 1,
+        allowChat: 1,
+        participantAllowed: 1,
+        endTime: 1,
+        startTime: 1,
+        stremingDate: 1,
+        selectProduct: '$hostproducts.product',
+        productId: '$hostproducts._id',
+        selectHost: 1,
+      },
+    },
+  ]);
+  return data;
+};
 
-  
-  const getAllLiveStremingDatasSame = async (id) => {
-    const data = await HostProduct.aggregate([
-      {
-        $match: {
-          $and: [{ uid: { $eq: id } }],
-        },
+const getAllLiveStremingDatasSame = async (id) => {
+  const data = await HostProduct.aggregate([
+    {
+      $match: {
+        $and: [{ uid: { $eq: id } }],
       },
-      {
-        $lookup: {
-          from: 'hostproducts',
-          localField: 'product',
-          foreignField: 'product',
-          as: 'hostproducts',
-        },
+    },
+    {
+      $lookup: {
+        from: 'hostproducts',
+        localField: 'product',
+        foreignField: 'product',
+        as: 'hostproducts',
       },
-      { $unwind: '$hostproducts' },
-      {
-        $lookup: {
-          from: 'hosts',
-          localField: 'hostproducts.uid',
-          pipeline:[
-            {
-              $match: {
-                $and: [{ category: { $eq: "host" } }],
-              },
+    },
+    { $unwind: '$hostproducts' },
+    {
+      $lookup: {
+        from: 'hosts',
+        localField: 'hostproducts.uid',
+        pipeline: [
+          {
+            $match: {
+              $and: [{ category: { $eq: 'host' } }],
             },
-          ],
-          foreignField: '_id',
-          as: 'hosts',
-        },
+          },
+        ],
+        foreignField: '_id',
+        as: 'hosts',
       },
-      { $unwind: '$hosts' },
-      {
-        $lookup: {
-          from: 'hoststreamings',
-          localField: 'hosts._id',
-          foreignField: 'selectHost',
-          as: 'hoststreamings',
-        },
+    },
+    { $unwind: '$hosts' },
+    {
+      $lookup: {
+        from: 'hoststreamings',
+        localField: 'hosts._id',
+        foreignField: 'selectHost',
+        as: 'hoststreamings',
       },
-      { $unwind: '$hoststreamings' },
-      {
-        $project:{
-          hostName:"$hosts.name",
-          selectProduct:"$hoststreamings.selectProduct",
-          stremingDate:"$hoststreamings.stremingDate",
-          selectHost:"$hoststreamings.selectHost",
-          _id1:"$hoststreamings._id",
-          startTime:"$hoststreamings.startTime",
-          endTime:"$hoststreamings.endTime",
-          participantAllowed:"$hoststreamings.participantAllowed",
-          allowChat:"$hoststreamings.allowChat",
-          token:"$hoststreamings.token",
-          efr:"$hostproducts.product"
-        }
+    },
+    { $unwind: '$hoststreamings' },
+    {
+      $project: {
+        hostName: '$hosts.name',
+        selectProduct: '$hoststreamings.selectProduct',
+        stremingDate: '$hoststreamings.stremingDate',
+        selectHost: '$hoststreamings.selectHost',
+        _id1: '$hoststreamings._id',
+        startTime: '$hoststreamings.startTime',
+        endTime: '$hoststreamings.endTime',
+        participantAllowed: '$hoststreamings.participantAllowed',
+        allowChat: '$hoststreamings.allowChat',
+        token: '$hoststreamings.token',
+        efr: '$hostproducts.product',
       },
+    },
+  ]);
+  return data;
+};
 
-
-    ])
-    return data;
-  };
-
-  const getAllproductById = async (id) =>{
-    const data = await HostProduct.aggregate([
-      {
-        $match: {
-          $and: [{ uid: { $eq: id } }],
-        },
+const getAllproductById = async (id) => {
+  const data = await HostProduct.aggregate([
+    {
+      $match: {
+        $and: [{ uid: { $eq: id } }],
       },
-    ])
-    return data
-  }
+    },
+  ]);
+  return data;
+};
 
-
-  const getAllStreaming = async (id) =>{
-    const data = await HostStreaming.aggregate([
-      {
-        $match: {
-          $and: [{ selectHost: { $eq: id } }],
-        },
+const getAllStreaming = async (id) => {
+  const data = await HostStreaming.aggregate([
+    {
+      $match: {
+        $and: [{ selectHost: { $eq: id } }],
       },
-    ])
-    return data
-  }
+    },
+  ]);
+  return data;
+};
 
-  const getAllStreamingToken = async (id) =>{
-    const data = await HostStreaming.aggregate([
-      {
-        $match: {
-          $and: [{ _id: { $eq: id } }],
-        },
+const getAllStreamingToken = async (id) => {
+  const data = await HostStreaming.aggregate([
+    {
+      $match: {
+        $and: [{ _id: { $eq: id } }],
       },
-    ])
-    return data
-  }
+    },
+  ]);
+  return data;
+};
 
-
-module.exports = { createHost, loginhostEmailAndPassword, createHostProduct, createHostStreaming, hostAll, getAllLiveStremingDatas,getAllLiveStremingDatasSame, getAllproductById, RecipentAll, getAllStreaming, getAllStreamingToken, getAll};
+module.exports = {
+  createHost,
+  loginhostEmailAndPassword,
+  createHostProduct,
+  createHostStreaming,
+  hostAll,
+  getAllLiveStremingDatas,
+  getAllLiveStremingDatasSame,
+  getAllproductById,
+  RecipentAll,
+  getAllStreaming,
+  getAllStreamingToken,
+  getAll,
+};
