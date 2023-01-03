8a40b709714f52888cc2689ec35abab479e7d0fa
muthamizhyadav <muthamizhyadav@gmail.com>
2023-01-03 17:52:10 +0530

    asdf

--- src/services/usersPlan.service.js
+++ src/services/usersPlan.service.js
@@ -7,7 +7,7 @@ const createUserPlan = async (body, id) => {
   let values = { ...body, ...{ created: moment(), userId: id } };
   let findByUsers = await usersPlan.findOne({ userId: id, active: true });
   if (findByUsers) {
-    throw new ApiError(httpStatus.BAD_GATEWAY, 'Exist plan Still in Active');
+    throw new ApiError(httpStatus.BAD_REQUEST, 'Exist plan Still in Active');
   }
   let data = await usersPlan.create(values);
   return data;
