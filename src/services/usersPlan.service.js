79ab76282765a3c2ab882782b0e0ddeb2e94233e
muthamizhyadav <muthamizhyadav@gmail.com>
2023-01-07 13:51:31 +0530

    asdf

--- src/services/usersPlan.service.js
+++ src/services/usersPlan.service.js
@@ -7,11 +7,6 @@ const moment = require('moment');
 const createUserPlan = async (body, id) => {
   let today = moment().toDate();
   let values = { ...body, ...{ created: moment(), userId: id } };
-  let plan = await AdminPlan.findById(body.PlanId);
-  console.log(plan.PlanValidate >= today);
-  if (plan.PlanValidate < today) {
-    throw new ApiError(httpStatus.NOT_ACCEPTABLE, 'Plan Expired');
-  }
   // if (plan.Amount !== body.Amount) {
   //   throw new ApiError(httpStatus.NOT_ACCEPTABLE, 'In Valid Amount');
   // }
