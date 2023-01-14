23a1d1cf43b403969fd27d1bbd2bb4097c39e901
muthamizhyadav <muthamizhyadav@gmail.com>
2023-01-14 11:04:01 +0530

    sdf

--- src/controllers/BuyerAuth.js
+++ src/controllers/BuyerAuth.js
@@ -19,7 +19,7 @@ const authorization = async (req, res, next) => {
   try {
     const payload = jwt.verify(token, config.jwt.secret);
     console.log(payload);
-    const userss = await Buyer.findOne({ _id: payload.sub });
+    const userss = await Buyer.findOne({ _id: payload.sub, active: true });
     if (!userss) {
       return res.send(httpStatus.UNAUTHORIZED, 'User Not Available');
     }
